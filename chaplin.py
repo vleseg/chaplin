42caec75f6e9d7e9c30f0e9e5500995fb232d7f7
vleseg <vlesiil@yandex.ru>
2014-02-21 00:12:10 +1000

    rewritten, like, everything

--- chaplin.py
+++ chaplin.py
@@ -1,152 +1,201 @@
+from collections import OrderedDict
+from itertools import chain
+
+
+class Case:
+    def __init__(self, path, footprint, documents):
+        self.footprint = footprint
+        self.paths = [path]
+        self.did_to_document = OrderedDict(zip(self.footprint, documents))
+
+    def __eq__(self, other):
+        return self.footprint == other.footprint
+
+    def get_paths_footprint(self):
+        return tuple([path.footprint for path in self.paths])
+
+    def add_path(self, path):
+        self.paths.append(path)
+
+    @staticmethod
+    def get_footprint_and_documents(path):
+        documents = list(chain(
+            *[answer.linked_documents for answer in path.get_answers()]
+        ))
+        return tuple(document.did for document in documents), documents
+
+
+class Cases:
+    def __init__(self):
+        self.paths_to_case = OrderedDict()
+        self.footprint_to_case = OrderedDict()
+
+    def handle_path(self, path):
+        footprint, documents = Case.get_footprint_and_documents(path)
+        if footprint not in self.footprint_to_case:
+            case = Case(path, footprint, documents)
+            self.footprint_to_case[footprint] = case
+        else:
+            case = self.footprint_to_case[footprint]
+            case.add_path()
+        self.paths_to_case[case.get_paths_footprint] = case
+
+
+class Path:
+    def __init__(self, raw_path):
+        self.footprint = tuple(answer.aid for answer in raw_path)
+        self.aid_to_answer = OrderedDict(zip(self.footprint, raw_path))
+        self.leads_to_rejection = raw_path[-1].is_rejection_clause
+
+    def __repr__(self):
+        return ' -> '.join(map(str, self.footprint))
+
+    def __str__(self):
+        return ' -> '.join(answer.short for answer in self.get_answers())
+
+    def get_answers(self):
+        return self.aid_to_answer.values()
+
+
+class Paths:
+    def __init__(self):
+        self.footprint_to_path = OrderedDict()
+
+    def add_path(self, raw_path):
+        path = Path(raw_path)
+        self.footprint_to_path[path.footprint] = path
+
+    def get_all(self):
+        return self.footprint_to_path.values()
+
+    def trim_rejections(self):
+        for path in self.values():
+            if path.leads_to_rejection:
+                del self.footprint_to_path[path.footprint]
+
+    def values(self):
+        return self.footprint_to_path.values()
+
+    def generate_cases(self):
+        cases = Cases()
+        for footprint in self.footprint_to_path:
+            cases.handle_path(self.footprint_to_path[footprint])
+
+        return cases
+
+
 class Answer:
-    def __init__(self, source):
-        self.aid = source["id"]
-        self.text = source["text"]
-        self.short = source["short"]
-        self.linked_question = None
+    def __init__(self, raw_answer, is_rejection_clause):
+        self.aid = raw_answer["id"]
+        self.text = raw_answer["text"]
+        self.short = raw_answer["short"]
+        self.is_rejection_clause = is_rejection_clause
+        # Will be filled later.
+        self.child_question = None
         self.linked_documents = []
 
     def attach_document(self, document):
         self.linked_documents.append(document)
 
 
+class Answers:
+    def __init__(self, raw_answers, rejections, aid_to_answer_common):
+        self.aid_to_answer = OrderedDict()
+
+        for raw_answer in raw_answers:
+            aid = raw_answer["id"]
+            is_rejection_clause = aid in rejections
+            self[aid] = Answer(raw_answer, is_rejection_clause)
+            aid_to_answer_common.update(self.aid_to_answer)
+
+    def __setitem__(self, key, value):
+        self.aid_to_answer[key] = value
+
+    def __iter__(self):
+        return self.aid_to_answer.__iter__()
+
+    def get_all(self):
+        return self.aid_to_answer.values()
+
+
 class Question:
-    def __init__(self, source):
-        self.aid_to_answer_mapping = {}
-        self.text = source["text"]
-        self.raw_linked_answers = source["linked_answers"]
-        self.linked_answers = None
-
-        for raw_a in source["answers"]:
-            self.aid_to_answer_mapping[raw_a["id"]] = Answer(raw_a)
-
-    def get_answers_dict(self):
-        return self.aid_to_answer_mapping
+    def __init__(self, raw_question, rejections, aid_to_answer_common):
+        self.qid = raw_question["id"]
+        self.text = raw_question["text"]
+        self.answers = Answers(
+            raw_question["answers"], rejections, aid_to_answer_common)
+        self.raw_parent_answers = raw_question["parent_answers"]
 
     def get_answers(self):
-        return self.aid_to_answer_mapping.values()
-
-
-class Case:
-    def __init__(self, raw_case):
-        self.case = raw_case
-
-    def __repr__(self):
-        return ' -> '.join([str(d.did) for d in self.case])
-
-    def __str__(self):
-        return ' -> '.join([d.text for d in self.case])
-
-
-class Path:
-    def __init__(self, seed):
-        self.path = seed
-        self.case = []
-
-    def __getitem__(self, item):
-        return self.path[item]
-
-    def __str__(self):
-        return ' -> '.join([a.short for a in self.path])
-
-    def __repr__(self):
-        return ' -> '.join([str(a.aid) for a in self.path])
-
-    def append(self, item):
-        self.path.append(item)
-
-    def generate_case(self):
-        reduction_lambda = lambda x, y: x + y.linked_documents
-        raw_case = reduce(reduction_lambda, self.path, [])
-        self.case = Case(raw_case)
-
-
-# TODO: implement unification of paths of identical cases at output time
-class Paths:
-    def __init__(self):
-        self.paths = []
-
-    def __getitem__(self, item):
-        return self.paths[item]
-
-    def append(self, item):
-        self.paths.append(item)
-
-    # Rejection clauses have to be a list of ints (aid).
-    def filter_out_rejections(self, rejection_clauses):
-        filter_func = lambda path: path[-1].aid not in rejection_clauses
-        self.paths = filter(filter_func, self.paths)
-
-    def generate_cases(self):
-        for p in self.paths:
-            p.generate_case()
+        return self.answers.get_all()
 
 
 class Questions:
-    def __init__(self, source):
-        self.questions = []
-        self.answers = {}
-        self.paths = None
+    def __init__(self, raw_questions, rejections):
+        self.qid_to_question = OrderedDict()
+        self.aid_to_answer = OrderedDict()
+        self.paths = Paths()
 
-        for raw_q in source:
-            self.questions.append(Question(raw_q))
-        for q in self.questions:
-            self.answers.update(q.get_answers_dict())
+        for raw_question in raw_questions:
+            question = Question(raw_question, rejections, self.aid_to_answer)
+            self[question.qid] = question
 
-    def __iter__(self):
-        return self.questions.__iter__()
+    def __setitem__(self, key, value):
+        self.qid_to_question[key] = value
 
-    def __getitem__(self, i):
-        return self.questions[i]
+    def __getitem__(self, item):
+        return self.qid_to_question[item]
 
-    def link_answers(self):
-        # First question already has a None Answer linked to it.
-        q_list = self.questions[1:]
-        for q in q_list:
-            q.linked_answers = [
-                self.answers[aid] for aid in q.raw_linked_answers]
-            for a in q.linked_answers:
-                a.linked_question = q
+    def get_all_aid_to_answer(self):
+        return self.aid_to_answer
+
+    def link_parent_answers(self):
+        for question in self.values():
+            if question.raw_parent_answers is not None:
+                for aid in question.raw_parent_answers:
+                    self.aid_to_answer[aid].child_question = question
+
+    def values(self):
+        return self.qid_to_question.values()
 
     def generate_paths(self):
         def recursive_helper(current_question, current_path):
-            for a in current_question.get_answers():
-                if a.linked_question is None:
-                    self.paths.append(Path(current_path + [a]))
+            for answer in current_question.get_answers():
+                if answer.child_question is None:
+                    self.paths.add_path(current_path + [answer])
                 else:
-                    recursive_helper(a.linked_question, current_path + [a])
+                    recursive_helper(
+                        answer.child_question, current_path + [answer])
 
-        self.paths = Paths()
-        recursive_helper(self.questions[0], [])
+        recursive_helper(self[0], [])
 
         return self.paths
 
 
 class Document:
-    def __init__(self, source):
-        self.did = source["id"]
-        self.text = source["text"]
-        self.raw_linked_answer = source["linked_answer"]
-        self.linked_answer = None
-
-    def get_linked_answer(self):
-        return self.linked_answer
+    def __init__(self, raw_document):
+        self.did = raw_document["id"]
+        self.text = raw_document["text"]
+        self.raw_linked_answers = raw_document["linked_answers"]
+        # Will be filled later.
+        self.linked_answers = []
 
 
 class Documents:
-    def __init__(self, source):
-        self.documents = []
+    def __init__(self, raw_documents):
+        self.did_to_document = OrderedDict()
 
-        for raw_d in source:
-            self.documents.append(Document(raw_d))
+        for raw_document in raw_documents:
+            document = Document(raw_document)
+            self[document.did] = document
 
-    def __iter__(self):
-        return self.documents.__iter__()
+    def __setitem__(self, key, value):
+        self.did_to_document[key] = value
 
-    def __getitem__(self, i):
-        return self.documents[i]
+    def values(self):
+        return self.did_to_document.values()
 
-    def link_actual_answers(self, answers):
-        for d in self.documents:
-            d.linked_answer = answers[d.raw_linked_answer]
-            d.linked_answer.attach_document(d)
+    def link_answers(self, aid_to_answer):
+        for document in self.values():
+            for aid in document.raw_linked_answers:
+                aid_to_answer[aid].attach_document(document)
--- chaplin_test.py
+++ chaplin_test.py
@@ -8,40 +8,41 @@ class TestChaplin(unittest.TestCase):
     def setUp(self):
         with open('test_schema.yaml', 'r') as f:
             source = yaml.load(f.read())
-        self.questions = Questions(source['questions'])
-        self.rejection_clauses = source['rejection_clauses']
+        self.questions = Questions(
+            source['questions'], source["rejection_clauses"])
         self.documents = Documents(source['documents'])
 
-        self.documents.link_actual_answers(self.questions.answers)
-        self.questions.link_answers()
+        self.documents.link_answers(self.questions.get_all_aid_to_answer())
+        self.questions.link_parent_answers()
 
     def testPathGeneration(self):
         paths = self.questions.generate_paths()
-        self.assertEqual(repr(paths[0]), '0 -> 2')
-        self.assertEqual(str(paths[1]), 'TA 1 -> TA 4 -> TA 5 -> TA 7')
-        self.assertEqual(repr(paths[2]), '0 -> 3 -> 4 -> 7')
-        self.assertEqual(str(paths[3]), 'TA 1 -> TA 4 -> TA 6 -> TA 7')
-        self.assertEqual(repr(paths[4]), '0 -> 3 -> 5 -> 7')
-        self.assertEqual(str(paths[5]), 'TA 2')
+        self.assertEqual(
+            [str(path) for path in paths.get_all()],
+            ['TA 1', 'TA 2 -> TA 4', 'TA 2 -> TA 5 -> TA 6 -> TA 8',
+             'TA 2 -> TA 5 -> TA 6 -> TA 9', 'TA 2 -> TA 5 -> TA 7 -> TA 8',
+             'TA 2 -> TA 5 -> TA 7 -> TA 9', 'TA 3']
+        )
+        self.assertEqual(
+            [repr(path) for path in paths.get_all()],
+            ['0', '1 -> 3', '1 -> 4 -> 5 -> 7', '1 -> 4 -> 5 -> 8',
+             '1 -> 4 -> 6 -> 7', '1 -> 4 -> 6 -> 8', '2']
+        )
 
     def testPathFiltering(self):
         paths = self.questions.generate_paths()
-        paths.filter_out_rejections(self.rejection_clauses)
-        self.assertEqual(str(paths[0]), 'TA 1 -> TA 3')
-        self.assertEqual(str(paths[1]), 'TA 1 -> TA 4 -> TA 5 -> TA 7')
-        self.assertEqual(str(paths[2]), 'TA 1 -> TA 4 -> TA 6 -> TA 7')
+        paths.trim_rejections()
+        self.assertEqual(
+            [str(path) for path in paths.get_all()],
+            ['TA 1', 'TA 2 -> TA 4', 'TA 2 -> TA 5 -> TA 6 -> TA 8',
+             'TA 2 -> TA 5 -> TA 7 -> TA 8']
+        )
+        self.assertEqual(
+            [repr(path) for path in paths.get_all()],
+            ['0', '1 -> 3', '1 -> 4 -> 5 -> 7', '1 -> 4 -> 6 -> 7']
+        )
 
-    def testDocuments(self):
-        paths = self.questions.generate_paths()
-        paths.filter_out_rejections(self.rejection_clauses)
-        paths.generate_cases()
-        test_str = lambda i: str(paths[i].case)
-        test_repr = lambda i: repr(paths[i].case)
-        self.assertEqual(test_str(0), 'Test document 1 -> Test document 2')
-        self.assertEqual(test_repr(1), '0 -> 2 -> 3')
-        self.assertEqual(test_str(2),
-            'Test document 1 -> Test document 3 -> Test document 5')
-
+    # TODO; Write test to check generation of Cases -- anew!
 
 if __name__ == "__main__":
     unittest.main()
